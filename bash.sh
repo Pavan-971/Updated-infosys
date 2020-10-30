4258da6572ba4aee397035c461d98b7ff626da9d
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2020-10-30 19:50:26 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,41 +5,16 @@ fileToBeRead="/var/www/html/sad.txt" #Whatever
 
 var=$(tr -d '[:blank:]\n' < $fileToBeRead)
 echo $var
-sed -i "20 s#.*header('Location:.*#header('Location: http://$var/second.html');#" details.php
+sed -i "20 s#.*header('Location:.*#header('Location: http://$var/second.html');#" /var/www/html/details.php
 
 
 fileToBeRead="/var/www/html/sad.txt" #Whatever
 
 var=$(tr -d '[:blank:]\n' < $fileToBeRead)
 echo $var
-sed -i "25 s#.*window.location.href=.*#window.location.href='http://$var/details.php'#"  details.php
+sed -i "25 s#.*window.location.href=.*#window.location.href='http://$var/details.php'#"  /var/www/html/details.php
 
 
-
-
-
-
-
-
-
-
-
-
-fileToBeRead="/var/www/html/sad.txt" #Whatever
-
-var=$(tr -d '[:blank:]\n' < $fileToBeRead)
-echo $var
-sed -i "7 s#.*header('Location:.*#header('Location: http://$var/firstpage.html');#" authen_login.php
-
-
-
-
-
-fileToBeRead="/var/www/html/sad.txt" #Whatever
-
-var=$(tr -d '[:blank:]\n' < $fileToBeRead)
-echo $var
-sed -i "13 s#.*header('Location:.*#header('Location: http://$var/balancepage.php');#" authen_login.php
 
 
 
@@ -54,7 +29,21 @@ fileToBeRead="/var/www/html/sad.txt" #Whatever
 
 var=$(tr -d '[:blank:]\n' < $fileToBeRead)
 echo $var
-sed -i "31 s#.*window.location.href=.*#window.location.href='http://$var/balancepage.php'#" balancedetails.php
+sed -i "7 s#.*header('Location:.*#header('Location: http://$var/firstpage.html');#" /var/www/html/authen_login.php
+
+
+
+
+
+fileToBeRead="/var/www/html/sad.txt" #Whatever
+
+var=$(tr -d '[:blank:]\n' < $fileToBeRead)
+echo $var
+sed -i "13 s#.*header('Location:.*#header('Location: http://$var/balancepage.php');#" /var/www/html/authen_login.php
+
+
+
+
 
 
 
@@ -65,19 +54,30 @@ fileToBeRead="/var/www/html/sad.txt" #Whatever
 
 var=$(tr -d '[:blank:]\n' < $fileToBeRead)
 echo $var
-sed -i '21 s#.*window.location.href=.*#window.location.href="'"http://$var/balancepage.php"'"#' balancedetails.php
+sed -i "31 s#.*window.location.href=.*#window.location.href='http://$var/balancepage.php'#" /var/www/html/balancedetails.php
 
-
-fileToBeRead="/var/www/html/sad.txt" #Whatever
-var=$(tr -d '[:blank:]\n' < $fileToBeRead)
-echo $var
-sed -i "30 s#.*window.location.href=.*#window.location.href='http://$var/second.html'#" accoun_auth_login.php
 
 
 
 
 
 fileToBeRead="/var/www/html/sad.txt" #Whatever
+
 var=$(tr -d '[:blank:]\n' < $fileToBeRead)
 echo $var
-sed -i "37 s#.*window.location.href=.*#window.location.href='http://$var/firstpage.html'#" accoun_auth_login.php
+sed -i '21 s#.*window.location.href=.*#window.location.href="'"http://$var/balancepage.php"'"#' /var/www/html/balancedetails.php
+
+
+fileToBeRead="/var/www/html/sad.txt" #Whatever
+var=$(tr -d '[:blank:]\n' < $fileToBeRead)
+echo $var
+sed -i "30 s#.*window.location.href=.*#window.location.href='http://$var/second.html'#" /var/www/html/accoun_auth_login.php
+
+
+
+
+
+fileToBeRead="/var/www/html/sad.txt" #Whatever
+var=$(tr -d '[:blank:]\n' < $fileToBeRead)
+echo $var
+sed -i "37 s#.*window.location.href=.*#window.location.href='http://$var/firstpage.html'#" /var/www/html/accoun_auth_login.php
